cc9b428167871e6290b50217000baa1ad414f7ba
bryan <bryanruiz0510@gmai.com>
2019-04-27 12:21:55 -0500

    condicional

--- primeros pasos1.py
+++ primeros pasos1.py
@@ -1,17 +1,17 @@
 # comentario de linea
 
-""" 
-comentario 
+import math
+
+"""Comentario 
 de parrafo
 """
-
 # hola mundo
 
 print("hello world")
 
 # variables
 
-x = 4   
+x = 4
 y = "Text"  # string
 z = 4.5     # float
 a = True    # booleano
@@ -40,3 +40,64 @@ x = str(2)
 x = str(2.8)
 print(x)
 x = str("2")
+# manejo de string en cadena de textos
+bebe = "HELLO WORLD"
+print(bebe[0])
+
+bebe = bebe.strip()
+print(len(bebe))
+print(bebe.lower)
+print(bebe.upper)
+
+# operaciones
+
+a = 2
+b = 3
+c = a + b
+c = a - b
+c = a * b
+c = a / b   # cociente
+c = a // b  # parte entera
+c = a % b
+c = a ** b  # exponente
+c = math.sqrt(a)  # raiz
+c = math.pi
+
+# captura por consola
+
+print("digite el nombre")
+nombre = input()
+print("hola"+nombre+"!")
+
+print("digite el #1")
+n1 = input()
+n1 = float(n1)
+print("digite el #2")
+n2 = input()
+n2 = float(n2)
+print(n1+n2)
+
+# condiciones
+
+a = 5
+b = 6
+
+if a > b:
+    print(a, "es mayor que", b)
+else:
+    print(b, "es mayor que", a)
+    
+if b > a :
+    if b > 1:
+        print(b, "es mayor que 1 y es mayor que", a)
+        
+ if a == b:
+     print("son iguales")     
+elif a > b:
+    print(a,"es mayor que",b)
+else:
+    print(b,"es mayor que",a)
+
+if a == b and a > 2:
+    print(a,"es igual a",b,"mayor que 2")
+        
