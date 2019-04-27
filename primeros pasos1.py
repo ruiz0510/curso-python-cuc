eaa37c45a82177eeee29f3caa7c2124acbd875ea
bryan <bryanruiz0510@gmai.com>
2019-04-27 10:42:18 -0500

    tipos de datos, conversiones

--- primeros pasos1.py
+++ primeros pasos1.py
@@ -1,4 +1,42 @@
+# comentario de linea
 
-a = 5
-b = 4
-c = a*b
+""" 
+comentario 
+de parrafo
+"""
+
+# hola mundo
+
+print("hello world")
+
+# variables
+
+x = 4   
+y = "Text"  # string
+z = 4.5     # float
+a = True    # booleano
+b = False
+c = 4+5j    # compleja
+
+print(x, y, z, a, b, c)
+
+# conocer el tipo de variable
+print(type(c))
+
+# conversiones de tipo de datos
+# enteros
+x = int(2.8)
+print(x)
+x = int("2")
+
+# float
+x = float(2)
+x = float(2.8)
+print(x)
+x = float("2")
+
+# string
+x = str(2)
+x = str(2.8)
+print(x)
+x = str("2")
